a306d839a61c53040e8ac02888d2e97c95877503
Aphiwe Mhotwana <127925737+ST10085670@users.noreply.github.com>
2025-03-03 11:29:14 +0200

    new controller

---
+++ prjWordleAPI/Controllers/WordleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjWordleAPI.Controllers
+{
+    public class WordleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
