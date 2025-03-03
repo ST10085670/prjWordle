353d799000cb710ad50348b97643d9a7b7b500cc
Aphiwe Mhotwana <127925737+ST10085670@users.noreply.github.com>
2025-03-03 11:39:52 +0200

    almost done

--- prjWordleAPI/Controllers/WordleController.cs
+++ prjWordleAPI/Controllers/WordleController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prjWordleAPI.Factory;
 
 namespace prjWordleAPI.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class WordleController : Controller
     {
-        public IActionResult Index()
+        [HttpGet("GetSingle")]
+        public String GetSingle(String lang)
         {
-            return View();
+            wordFactory langFactory = new wordFactory();
+            iWord langInstance = langFactory.GetLang(lang);
+            word w = word.getInstance();
+            return w.Single(langInstance.getName());
         }
     }
 }
---
+++ prjWordleAPI/Factory/word.cs
@@ -0,0 +1,19 @@
+﻿namespace prjWordleAPI.Factory
+{
+    private static word instance;
+    private word() { }
+
+    public static word getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new word();
+        }
+        return instance;
+    }
+    public String Random(String[] arrWords)
+    {
+        Random rnd = new Random();
+        return arrWords[rnd.Next(arrWords.Length - 1)];
+    }
+}
