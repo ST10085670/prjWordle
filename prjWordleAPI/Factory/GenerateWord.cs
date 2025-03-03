bb30ad84b6ac859f71ba947043aab0123e03d82c
Aphiwe Mhotwana <127925737+ST10085670@users.noreply.github.com>
2025-03-03 11:01:44 +0200

    array list

---
+++ prjWordleAPI/Factory/GenerateWord.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+namespace prjWordleAPI.Factory
+{
+    public class GenerateWord : iWord
+    {
+        public ArrayList arrWords = new ArrayList();
+        public string[] genWord()
+        {
+
+            arrWords.Add()
+            return arrWords;
+        }
+    }
+}
